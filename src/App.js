f0c524fda6f61d2e6dea76b127f35b23f5ef1445
JuanDavid1217 <zs20006756@estudiantes.uv.mx>
2023-04-04 14:22:14 -0600

    se sube

--- src/App.js
+++ src/App.js
@@ -7,9 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-	  <br/>CreazyDave
-	  <br/>Hola
+          Rick Morty App 
+	  By CreazyDave ...
         </p>
         <a
           className="App-link"
